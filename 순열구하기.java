c04525d270ac12511a56358a1ee946ae020bde64
hokyunJung <everlips@naver.com>
2022-08-01 17:09:05 +0900

    Create 순열구하기.java
    순열 구하기
    10이하의 N개의 자연수가 주어지면 이 중 M개를 뽑아 일렬로 나열하는 방법을 모두 출력합
    니다.
    ▣ 입력설명
    첫 번째 줄에 자연수 N(3<=N<=10)과 M(2<=M<=N) 이 주어집니다.
    두 번째 줄에 N개의 자연수가 오름차순으로 주어집니다.
    ▣ 출력설명
    첫 번째 줄에 결과를 출력합니다.
    출력순서는 사전순으로 오름차순으로 출력합니다.
    ▣ 입력예제 1
    3 2
    3 6 9
    ▣ 출력예제 1
    3 6
    3 9
    6 3
    6 9
    9 3
    9 6

---
+++ 순열구하기.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Main {
+
+    static int[] pm;
+    static int[] ch;
+    static int[] arr;
+    static int n;
+    static int m;
+
+
+    public void DFS(int L) {
+        if (L == m) {
+            for (int x : pm) System.out.print(x + " ");
+            System.out.println();
+            return;
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    pm[L] = arr[i];
+                    DFS(L+1);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    /*
+                                []
+L=0        pm[3]              pm[6]               pm[9]
+        ch[1,0,0]           ch[0,1,0]           ch[0,0,1]
+
+L=1   pm[3,6]  pm[3,9]    pm[6,3]  pm[6,9]     pm[9,3]  pm[9,6]
+     ch[1,1,0] ch[1,0,1] ch[1,1,0] ch[0,1,1]  ch[1,0,1] ch[0,1,1]
+
+L=2 print pm[3,6]
+
+
+     */
+
+    public static void main(String[] args) {
+        Main T = new Main();
+
+        Scanner kb = new Scanner(System.in);
+
+        n = kb.nextInt();
+        arr = new int[n];
+        m = kb.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        pm = new int[m];
+        ch = new int[n];
+
+        T.DFS(0);
+    }
+}
