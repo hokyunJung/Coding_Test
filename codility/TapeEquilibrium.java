b87ea0d4c50f467fcaa3924354b29da65607bf89
hokyunJung <everlips@naver.com>
2023-06-13 21:45:31 +0900

    Create TapeEquilibrium.java

---
+++ codility/TapeEquilibrium.java
@@ -0,0 +1,56 @@
+A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+
+Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+
+The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+
+In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+
+For example, consider array A such that:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+We can split this tape in four places:
+
+P = 1, difference = |3 − 10| = 7
+P = 2, difference = |4 − 9| = 5
+P = 3, difference = |6 − 7| = 1
+P = 4, difference = |10 − 3| = 7
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+
+For example, given:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+the function should return 1, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [2..100,000];
+each element of array A is an integer within the range [−1,000..1,000].        
+
+        
+        int min = Integer.MAX_VALUE;
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        for(int i : A) sum+=i; //총합을 미리 구한다
+
+        for(int p = 1; p < A.length; p++) {
+            left += A[p-1]; //left는 한칸식 이동하면서 합해준다
+            right = sum - left; //right 는 총합에서 left를 빼준다.
+            
+            if (min > Math.abs(left - right)) min = Math.abs(left - right);
+        }
+
+        return min;
